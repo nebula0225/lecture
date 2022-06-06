83791970ab1a217662e69b05d92f7fe724ca27fd
HeidongH <qkqajfl007@naver.com>
2022-06-06 16:26:50 +0900

    22.06.06 주요 내용 : 자바 1.8이상 사용 가능한 Optional Stream 람다 활용식

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,48 @@
+package hello.hellospring.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import hello.hellospring.domain.Member;
+
+public class MemoryMemberRepository implements MemberRepository {
+	
+	private static Map<Long, Member> store = new HashMap<>();
+	private static long sequence = 0L;
+
+	@Override
+	public Member save(Member member) {
+		// TODO Auto-generated method stub
+		member.setId(++sequence);
+		store.put(member.getId(), member);
+		return member;
+	}
+
+	@Override
+	public Optional<Member> findById(Long id) {
+		// TODO Auto-generated method stub
+		return Optional.ofNullable(store.get(id));
+		// ofNullable : 값이 null 일수도 아닐수도 있는 경우에 사용
+	}
+
+	@Override
+	public Optional<Member> findByName(String name) {
+		// TODO Auto-generated method stub
+		return store.values().stream()
+				.filter(member -> member.getName().equals(name))
+				.findAny();
+		// filter를 쓰기 위해서는 stream 람다를 사용해야함. (filter는 람다식 함수)
+		// Stream을 직렬 처리할때 findAny()는 Stream에서 가장 먼저 탐색되는 요소를 리턴함.
+		// Stream을 병렬 처리할때는 findAny()가 리턴하는 값이 매번 달라짐.
+	}
+
+	@Override
+	public List<Member> findAll() {
+		// TODO Auto-generated method stub
+		return new ArrayList<>(store.values());
+	}
+
+}
