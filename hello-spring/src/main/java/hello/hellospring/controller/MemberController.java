7e4a45c7480d823fd211327bff4d02a7870f8813
nebula0225 <qkqajfl007@naver.com>
2022-07-26 20:26:31 +0900

    h2 DB 설치 전 단계

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,13 @@
 package hello.hellospring.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 
 @Controller
@@ -10,9 +15,32 @@ public class MemberController {
 	
 	private final MemberService memberService;
 
-	@Autowired
+	// 단일 생성자인 경우에는 어노테이션 안붙여도 됨.
 	public MemberController(MemberService memberService) {
 		this.memberService = memberService;
+	}
+	
+	@GetMapping("/members/new")
+	public String createForm() {
+		return "members/createMemberForm";
+	}
+	
+	@PostMapping("/members/new")
+	public String create(MemberForm form) {
+		Member member = new Member();
+		
+		member.setName(form.getName());
+		memberService.join(member);
+		
+		return "redirect:/";
+	}
+	
+	@GetMapping("/members")
+	public String list(Model model) {
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members", members);
+		
+		return "members/memberList";
 	}
 	
 	
